b06bdb07c6388542e8c602ab1bb94866b094385e
marcialtg <marcialtg@gmail.com>
2026-06-23 10:42:16 +0200

    Cambiar modelo a gemma2-9b-it

--- netlify/functions/groq.js
+++ netlify/functions/groq.js
@@ -1,5 +1,5 @@
 const GROQ_KEY = process.env.GROQ_API_KEY;
-const GROQ_MODEL = 'llama-3.3-70b-versatile';
+const GROQ_MODEL = 'gemma2-9b-it';
 
 exports.handler = async (event) => {
   const headers = {
